51810e23e48d5bb7e5f9ab30af12927e1f81577b
kumarramjee <krram1991@gmail.com>
2015-07-15 12:55:37 +0530

    initial Commimt

--- app/src/main/java/materialdesignstyle/mobimedia/com/materialdesignstyles/ui/ui/Watsappactivity.java
+++ app/src/main/java/materialdesignstyle/mobimedia/com/materialdesignstyles/ui/ui/Watsappactivity.java
@@ -2,6 +2,7 @@ package materialdesignstyle.mobimedia.com.materialdesignstyles.ui.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toolbar;
@@ -23,13 +24,26 @@ public class Watsappactivity extends Activity implements ObservableScrollViewCal
     private ImageView image;
     private HorizontalListView mHorizontallistview;
 
+    private View mFab;
+    private int mActionBarSize;
+private int imageheight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watsappactivity);
         SetUpUI();
+
+
         mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
         mFlexibleSpaceShowFabOffset = getResources().getDimensionPixelSize(R.dimen.flexible_space_show_fab_offset);
+
+
+
+
+        imageheight=image.getMaxHeight();
+        Log.i("Height of image is:","=="+imageheight);
+
+
 
 
     }
@@ -40,6 +54,17 @@ public class Watsappactivity extends Activity implements ObservableScrollViewCal
         mHorizontallistview = (HorizontalListView) findViewById(R.id.horizontalimagelist);
     }
 
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageback:
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
@@ -54,15 +79,5 @@ public class Watsappactivity extends Activity implements ObservableScrollViewCal
     @Override
     public void onUpOrCancelMotionEvent(ScrollState scrollState) {
 
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.imageback:
-                break;
-            default:
-                break;
-        }
     }
 }
